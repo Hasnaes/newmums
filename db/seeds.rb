1331d176da9a748216677f45910630b7fd901e2a
HasnaeSn <hasnae.sdn@outlook.com>
2023-08-29 14:35:30 +0200

    finished initialization

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Creating activities"
+Activity.destroy_all
+
+Activity.create(name: "monde d'ayden", location: "Brussel", capacity: "5", price: "10", description: "super cool and intersting activity for newmums", start_date: "1er october 2023", end_date: "5 october 2023")

ef0be56e995dcc66ef49d1f73c9301669af147bc
HasnaeSn <hasnae.sdn@outlook.com>
2023-08-31 17:45:33 +0200

    updates validation

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -12,8 +12,8 @@ class Activity < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true, length: { minimum: 40 }
   validates :capacity, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  # validates :price, presence: true, numericality: { greater_than: 0 }
   validates :location, presence: true
-  validates :photo, presence: true
+  # validates :photo, presence: true
   validates :category, presence: true
 end

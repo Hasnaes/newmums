437af6ef9496013571b3a717f78fbc5d79170ff6
Leo Thompson <leo.francis.thompson@gmail.com>
2023-08-31 17:29:10 +0200

    added messaging system with live updates, adjusted forms

---
+++ app/javascript/controllers/chatroom_subscription_controller.js
@@ -0,0 +1,28 @@
+import { Controller } from "@hotwired/stimulus"
+import { createConsumer } from "@rails/actioncable"
+
+
+export default class extends Controller {
+  static values = { chatroomId: Number }
+  static targets = ["messages"]
+
+  connect() {
+    this.channel = createConsumer().subscriptions.create(
+      { channel: "ChatroomChannel", id: this.chatroomIdValue },
+      { received: data => this.messagesTarget.insertAdjacentHTML("beforeend", data),
+        received: data => this.#insertMessageAndScrollDown(data)
+    }
+    )
+    console.log(`Subscribe to the chatroom with the id ${this.chatroomIdValue}.`)
+  }
+
+  #insertMessageAndScrollDown(data) {
+    this.messagesTarget.insertAdjacentHTML("beforeend", data)
+    this.messagesTarget.scrollTo(0, this.messagesTarget.scrollHeight)
+  }
+
+  resetForm(event) {
+    console.log("hello im here what up")
+    event.target.reset()
+  }
+}

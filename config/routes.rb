090e70d6f9226ba95b092f74fa2b1c2825450277
Leo Thompson <leo.francis.thompson@gmail.com>
2023-09-06 12:20:19 +0200

    addingnews

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
 
+  resources :news, only: [:index, :show]
   resources :activities, only: [:index, :show] do
     resources :participations, only: [:create]
     resources :reviews, only: [:create]
